eb35beef9a5680d9bc80564b50c9c0e8ce522fc2
jahidsami985 <jahidsami561@gmail.com>
2026-06-18 23:37:04 -0700

    Fix missing Bootstrap source map for Render

--- ecom/settings.py
+++ ecom/settings.py
@@ -152,10 +152,9 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
-
 
 # Media files
 
