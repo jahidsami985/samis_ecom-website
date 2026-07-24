4d5d719c1fefcc0403b766eea6c6a713ab2333fa
jahidsami985 <jahidsami561@gmail.com>
2026-07-24 07:41:06 -0700

    Log database target during Render build

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
+echo "Using database: ${MYSQL_USER:-<unset>}@${MYSQL_HOST:-<unset>}:${MYSQL_PORT:-<unset>}/${MYSQL_DATABASE:-<unset>} (ssl=${MYSQL_SSL_MODE:-<unset>})"
 python manage.py migrate --noinput
